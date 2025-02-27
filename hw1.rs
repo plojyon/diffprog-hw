4652c054bf3401725018e8d5cd544ba074bcaecc
Yon Ploj <admin@yon.si>
2025-02-27 15:11:17 +0100

    Apply rustfmt

--- hw1.rs
+++ hw1.rs
@@ -1,12 +1,22 @@
 fn main() {
     println!("Hello, world!");
 
-    let f = &|x: &[f64]| x.iter().map(|&i| i.powi(3)).collect(); // f(x) = x^3, f'(x) = 3x^2
-    let derivative = numerical_derivative(f, &[6.0], None); // f'(6) = 3*6^2 = 108
+    // f(x) = x^3
+    // f'(x) = 3x^2
+    // f'(6) = 3*6^2 = 108
+    let f = &|x: &[f64]| x.iter().map(|&i| i.powi(3)).collect();
+    let derivative = numerical_derivative(f, &[6.0], None);
     println!("Expected: 108, calculated: {derivative:?}");
 
-    let f = &|x: &[f64]| x.iter().map(|&i| i.powi(3) + 4.0*i.powi(2) - 12.0).collect(); // f(x) = x^3 + 4x^2 - 12, f'(x) = 3x^2 + 8x
-    let derivative = numerical_derivative(f, &[2.0], Some(1e-5)); // f'(2) = 3*2^2 + 8*2 = 28
+    // f(x) = x^3 + 4x^2 - 12
+    // f'(x) = 3x^2 + 8x
+    // f'(2) = 3*2^2 + 8*2 = 28
+    let f = &|x: &[f64]| {
+        x.iter()
+            .map(|&i| i.powi(3) + 4.0 * i.powi(2) - 12.0)
+            .collect()
+    };
+    let derivative = numerical_derivative(f, &[2.0], Some(1e-5));
     println!("Expected: 28, calculated: {derivative:?}");
 }
 
@@ -14,5 +24,9 @@ fn main() {
 fn numerical_derivative(f: &dyn Fn(&[f64]) -> Vec<f64>, x: &[f64], h: Option<f64>) -> Vec<f64> {
     let h = h.unwrap_or(1e-5);
     let x_h: Vec<f64> = x.iter().map(|&i| i + h).collect(); // x + h
-    f(&x_h).iter().zip(f(x).iter()).map(|(a, b)| (a - b) / h).collect() // (f(x + h) - f(x)) / h
+    f(&x_h)
+        .iter()
+        .zip(f(x).iter())
+        .map(|(a, b)| (a - b) / h)
+        .collect()
 }
